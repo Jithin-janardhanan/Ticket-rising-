29fa0a2f7f51ce7dd22ed97d6b68709b3aa62873
Jithin-janardhanan <jithinjanardhanan97@gmail.com>
2025-10-09 10:59:54 +0530

    almost completed

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,6 @@ import TicketsPage from "./users/pages/ticket_form/ticketpage";
 import TicketFormPage from "./users/pages/ticket_form/ticketformpages";
 import RaiseTicket from "./users/pages/guest_tickets/raise_ticket";
 import ForgotPassword from "./users/pages/forgotpassword/request_otp";
-// Layout (with Sidebar always shown)
 import DashboardLayout from "./users/pages/dashboard/dashboard";
 import TicketDetailPage from "./users/pages/ticket_detials/ticket_details";
 
@@ -24,13 +23,13 @@ function App() {
           {/* Old dashboard route (if you still need it) */}
           <Route path="/helpdesk-dashboard" element={<HelpdeskDashboard />} />
           <Route path="/raise-ticket" element={<RaiseTicket />} />
-
           {/* Protected routes with Sidebar layout */}
           <Route element={<DashboardLayout />}>
             <Route path="/ProfilePage" element={<ProfilePage />} />
             <Route path="/ticket/new" element={<TicketFormPage />} />
             <Route path="/tickets" element={<TicketsPage />} />
              <Route path="/ticketdetials" element={<TicketDetailPage />} />
+             <Route path="/tickets/:ticketId" element={<TicketDetailPage />} />
           </Route>
 
           {/* Default route */}
